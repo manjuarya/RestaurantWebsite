d90d2c54f9e0ba07c8e9047f2632ecaf7b5c48d4
manju <mann.arya@gmail.com>
2020-09-09 17:38:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigureStore } from './redux/configureStore';
+import Main from './components/MainComponent';
+import './App.css';
+
+
+
+const store = ConfigureStore();
+
+class App extends Component {
+
+  render(){
+
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
